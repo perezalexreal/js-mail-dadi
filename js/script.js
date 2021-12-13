636a03336050d9ab223f78b5939dac04b194c007
perezalexreal <perezalexreal@gmail.com>
2021-12-13 17:43:40 +0100

    finito

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-const emailUser = prompt("Inserisci la tua mail");
+// const emailUser = prompt("Inserisci la tua mail");
  
-const dbEmail = ["test1@gmail.com", "test2@gmail.com", "test3@gmail.com", "test4@gmail.com", "test5@gmail.com", "test6@gmail.com" ]
+// const dbEmail = ["test1@gmail.com", "test2@gmail.com", "test3@gmail.com", "test4@gmail.com", "test5@gmail.com", "test6@gmail.com" ]
 
 
- let logged = false;
+//  let logged = false;
 
- for(let i = 0 ; i < dbEmail.length ; i++) {
-    if( emailUser === dbEmail[i]){
-        logged  = true;
-    }
- }
+//  for(let i = 0 ; i < dbEmail.length ; i++) {
+//     if( emailUser === dbEmail[i]){
+//         logged  = true;
+//     }
+//  }
 
- if (logged) {
-    alert("Sei entrato con successo");
-    console.log("Sei entrato con successo");
- } else {
-    alert("Non hai inserito l'email giusta");
-     console.log("Non hai inserito l'email giusta");
- }
+//  if (logged) {
+//     alert("Sei entrato con successo");
+//     console.log("Sei entrato con successo");
+//  } else {
+//     alert("Non hai inserito l'email giusta");
+//      console.log("Non hai inserito l'email giusta");
+//  }
+
+console.log("kkd");
