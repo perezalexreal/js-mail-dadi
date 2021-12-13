f2b6b8aa3344fd009dbfae861424c1e692a12daf
perezalexreal <perezalexreal@gmail.com>
2021-12-13 17:17:04 +0100

    Primo esercizio svolto

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const emailUser = prompt("Inserisci la tua mail");
+ 
+const dbEmail = ["test1@gmail.com", "test2@gmail.com", "test3@gmail.com", "test4@gmail.com", "test5@gmail.com", "test6@gmail.com" ]
+
+
+ let logged = false;
+
+ for(let i = 0 ; i < dbEmail.length ; i++) {
+    if( emailUser === dbEmail[i]){
+        logged  = true;
+    }
+ }
+
+ if (logged) {
+    alert("Sei entrato con successo");
+    console.log("Sei entrato con successo");
+ } else {
+    alert("Non hai inserito l'email giusta");
+     console.log("Non hai inserito l'email giusta");
+ }
